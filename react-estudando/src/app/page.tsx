bd4cab744a176f25bbb5acd5b961debb1a5c47be
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2024-05-24 16:36:23 -0300

    Listas-Renderizacao

--- react-estudando/src/app/page.tsx
+++ react-estudando/src/app/page.tsx
@@ -2,16 +2,22 @@ import { Card } from "@/components/Card"
 import { GeoForm } from "@/components/GeoForm"
 import { Person } from "@/components/Person"
 import { url } from "inspector"
+import { peopleList } from "@/data/peopleList"
 
 const Page = () => {
+
   return(
-    <div>
-      <Card
-        phrase="Frase de Motivação"
-        author="jean"
-      />
+    <div className="text-3xl">
+      {peopleList.length > 0 &&
+        <ul>
+        {peopleList.map(person => 
+          <li key={person.id}>{person.name} - {person.profession}</li>
+        )}
+      </ul>
+      }
     </div>
   )
+
 }
 
 export default Page
---
+++ react-estudando/src/data/peopleList.ts
@@ -0,0 +1,9 @@
+import { Person } from "@/types/Person";
+
+export const peopleList: Person[] = [
+    { id: 1, name: "jean", profession: "mathematician"},
+    { id: 2, name: "ciclano", profession: "corredor"},
+    { id: 3, name: "fulano", profession: "motorista"},
+    { id: 4, name: "aiando", profession: "uber"},
+    { id: 5, name: "deiando", profession: "motoboy"}
+]
