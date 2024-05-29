b6aaaa2f1f472077bee074249edff4adc764149d
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2024-05-29 17:13:16 -0300

    Exercicio-tabelaNotas

--- react-estudando/src/components/StudentTable.tsx
+++ react-estudando/src/components/StudentTable.tsx
@@ -19,7 +19,31 @@ export const StudentTable = ({students}: Props) => {
             </thead>
 
             <tbody>
+                {students.map(people => (
+                    <tr key={people.id} className="text-gray-800 bg-gray-400 border-b border-gray-600">
+                        <td className="flex p-3 items-center">
+                            <img src={people.avatar} alt={people.name} className="w-10 h-10 rounded-full mr-3"/>
+                            <div>
+                                <div className="font-bold">{people.name}</div>
+                                <div>{people.email}</div>
+                            </div>
+                        </td>
+                        <td className="p-3">
+                            {people.active && 
+                                <div className="px-2 py-1 inline-block rounded-md border border-green-800 bg-green-600 text-white text-xs">
+                                    Active
+                                </div>}
 
+                            {!people.active && 
+                                <div className="px-2 py-1 inline-block rounded-md border border-red-800 bg-red-600 text-white text-xs">
+                                    Inactive
+                                </div>}
+                        </td>
+                        <td className="p-3">{people.grade1.toFixed(1)}</td>
+                        <td className="p-3">{people.grade2.toFixed(1)}</td>
+                        <td className="p-3 font-bold">{people.active ? ((people.grade1 + people.grade2) / 2).toFixed(1) : "--"}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     )
