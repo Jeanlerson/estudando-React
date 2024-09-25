01d33108c02846e4a32aaf0e7157b820a0990729
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2024-09-25 20:50:29 -0300

    preventDefault-react

--- react-estudando/src/app/page.tsx
+++ react-estudando/src/app/page.tsx
@@ -1,38 +1,38 @@
 "use client"
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { CustomButton } from "@/components/CunstomButton";
 
 const Page = () => {
   function handleClick() {
     alert("Funcionou!")
-  }
+  };
   const avisar = (msg: string) => {
     alert(msg)
-  }
+  };
 
   const [backgroundColor, setBackgroundColor] = useState("bg-gradient-to-r from-sky-500 to-indigo-500");
   const changeBackground = (colorClass: string) => {
     setBackgroundColor(colorClass)
+  };
+
+  const handleButton1 = () => changeBackground("bg-red-500");
+  const handleButton2 = () => changeBackground("bg-green-500");
+  const handleButton3 = () => changeBackground("bg-yellow-500");
+
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    alert("Mandando")
   }
-
-  const handleButton1 = () => changeBackground("bg-red-500")
-  const handleButton2 = () => changeBackground("bg-green-500")
-  const handleButton3 = () => changeBackground("bg-yellow-500")
-
   return (
    <div className={`w-screen h-screen flex flex-col justify-center
    items-center text-white ${backgroundColor}`}> 
-      <div className="bg-red-700 p-5 flex flex-col">
-        <button onClick={handleClick} className="p-3 bg-blue-700 rounded-md mb-3">Clique Aqui</button>
-        <button onClick={() => alert("Funcionou 2.0!")} className="p-3 bg-blue-700 rounded-md">Clique Aqui 2.0</button>
-        <button onClick={() => avisar('Mensagem Inserida')} className="p-3 bg-blue-700 rounded-md mt-3">Clique Aqui 3</button>
-      </div>
-
-      <CustomButton label="Clique Aqui Diferenciado 1" onClick={handleButton1}/>
-      <CustomButton label="Clique Aqui Diferenciado 2" onClick={handleButton2}/>
-      <CustomButton label="Clique Aqui Diferenciado 3" onClick={handleButton3}/>
+      <h1 className="text-5xl mb-3">Form de Login</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" />
+        <input className="ml-3" type="submit" value="Enviar"/>
+      </form>
    </div>
-  )
+  );
   
 };
 
