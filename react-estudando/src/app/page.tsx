9c5dec34716ded84a8a516e83f8d86d1b5afe173
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2024-10-15 17:36:57 -0300

    atl-limpando

--- react-estudando/src/app/page.tsx
+++ react-estudando/src/app/page.tsx
@@ -45,46 +45,11 @@ const Page = () => {
   
   */
 
-  const [fullName, setFullName] = useState<FullName>({ name: '', lastName: '' })
 
-  const handleClearButtonName = () => {
-    setFullName({ ...fullName, name: '' })
-  }
-  const handleClearButtonLastName = () => {
-    const fullNameClone = { ...fullName }
-    fullNameClone.lastName = ''
-
-    setFullName(fullNameClone)
-  }
-
-  const [list, setList] = useState<TodoItem[]>([
-    {label: 'Fazer trabalho escolar', checked: false},
-    {label: 'Estudar', checked: true},
-    {label: 'Fazer compras', checked: false}
-  ]);
 
   return (
 
-    <div className="w-screen h-screen flex flex-col justify-center
-    items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
-      <h1 className="text-4xl mt-5">Lista Tarefa</h1>
-
-      <div className="flex w-full max-w-lg my-3 p-4 rounded-md bg-gray-700 border-2 border-grayt-700">
-        <input 
-          type="text"
-          placeholder="O que deseja fazer?"
-          className="felx-1 border border-black p-3 text-2xl text-black rounded-md mr-3"
-        />
-
-        <button>Adicionar</button>
-      </div>
-
-      <ul className="w-full max-w-lg list-disc pl-5">
-        {list.map(item => (
-          <li>{item.label} - <button className="hover:underline text-red-700 font-bold">[ deletar ]</button></li>
-        ))}
-      </ul>
-    </div>
+    <div></div>
 
     /*
    <div className={`w-screen h-screen flex flex-col justify-center
