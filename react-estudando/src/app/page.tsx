07b5b98e84a5fc56063f76cb1dbbe5bac9babd1f
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2024-10-22 22:55:16 -0300

    atl-background

--- react-estudando/src/app/page.tsx
+++ react-estudando/src/app/page.tsx
@@ -50,12 +50,12 @@ const Page = () => {
 
   return (
 
-    <div className="mx-2">
+    <div className="mx-2 bg-gradient-to-r from-sky-500 to-indigo-500">
       <h1 className="text-center text-3xl font-bold my-10">Fotos Intergalactios</h1>
 
       <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {photoList.map(item => (
-          <div>{item.url}</div>
+          <div>{item.url }</div>
         ))}
       </section>
     </div>
