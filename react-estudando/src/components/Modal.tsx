93f653860a87f835c29469e2c2e3a43d55ef6078
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2024-11-21 18:19:59 -0300

    add-funcao-closeModal

--- react-estudando/src/components/Modal.tsx
+++ react-estudando/src/components/Modal.tsx
@@ -9,7 +9,7 @@ export const Modal = ({image, closeModal}: Props) => {
             <div className="fixed left-0 top-0 right-0 bottom-0 flex justify-center items-center bg-black/90">
                 <img src={`/assets/img/${image}`} alt="" className="max-w-screen max-h-screen"/>
             </div>
-            <div className="fixed top-5 right-5 w-10 h-10 cursor-pointer text-whit text-5xl">
+            <div onClick={closeModal} className="fixed top-5 right-5 w-10 h-10 cursor-pointer text-whit text-5xl text-red-700">
                 x
             </div>
         </>
