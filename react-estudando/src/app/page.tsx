48ae00a112ce64efef55173c0d35205a70d3bc7b
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-01-03 23:27:12 -0300

    usandoReducer-1

--- react-estudando/src/app/page.tsx
+++ react-estudando/src/app/page.tsx
@@ -106,11 +106,43 @@ const Page = () => {
 
 //  const [show, setShow] = useState(false);
 
-  const [list, dispatch] = useReducer(listReducer, [])
+  const [list, dispatch] = useReducer(listReducer, []);
+
+  const [addField, setAddField] = useState('');
+
+  const handleAddButton = () => {
+    if(addField.trim() === '') return false;
+
+    dispatch({
+      type: 'add',
+      payload: {
+        text: addField.trim()
+      }
+    })
+
+    setAddField('');
+  }
 
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center bg-blue-600">
-      ...
+    <div className="container mx-auto">
+      <h1 className="text-center text-4xl my-4">Lista de Tarefas</h1>
+      <div className="max-w-2xl mx-auto flex rounded-md border bg-gray-900 border-gray-400 p-4 my-4">
+        <input 
+          type="text" 
+          className="flex-1 rounded-md border border-white p-3 bg-transparent text-white outline-none"
+          placeholder="Digite um item"
+          value={addField}
+          onChange={e => setAddField(e.target.value)} />
+        <button
+          className="p-4"
+          onClick={handleAddButton}
+        >ADICIONAR</button>
+      </div>
+      <ul>
+        {list.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     </div>
     /* ------------------- USE EFFECT----------------------
 
